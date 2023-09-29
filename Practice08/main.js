1df10f046ffd32b39f221752cf62274b1f90040a
pascenby <pascenby@rose-gulman.edu>
2023-09-29 14:30:36 -0400

    finished 08

---
+++ Practice08/main.js
@@ -0,0 +1,57 @@
+function mult(x, y){
+return x*y;
+}
+
+function isEven(num){
+return   num%2 == 0;
+
+
+
+}
+
+function subtract(a,b){
+    return b - a;
+}
+const difference = subtract(2,3);
+
+function greet(name){
+    return `Hello, ${name}!`;
+}
+const greeting = greet('Bobby');
+
+const calculate = function(x,y) {
+    return x**2 + y**2;
+};
+
+function average(num1, num2, num3){
+    return (num1+num2+num3)/3;
+}
+
+multiply = (a,b) => a*b;
+
+greet1 = (name)  => `Hello, ${name}!`;
+
+square = (x) => x*x;
+
+isEven1 = (num) => num%2 ===0;
+
+findMax = (numbers) => Math.max(...numbers);
+
+addTwoNumbers = (a,b) =>  sum = a+b;
+
+console.log(mult(3,4));
+console.log(isEven(8));
+console.log(difference);
+console.log(greeting);
+console.log(calculate(2,3));
+console.log(average(1,2,3));
+console.log(multiply(2,3))
+console.log(greet1('greg'));
+console.log(square(4));
+console.log(isEven1(6));
+const numberL= [1,2,3];
+console.log(findMax(numberL))
+console.log(addTwoNumbers(2,3))
+
+
+
