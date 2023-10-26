671aa412da9767571a3c9585301e0623b03173c1
pascenby <pascenby@rose-gulman.edu>
2023-10-26 13:38:35 -0400

    Did postman

--- ClientsTestimonial/public/scripts/main.js
+++ ClientsTestimonial/public/scripts/main.js
@@ -1,5 +1,5 @@
 var rhit = rhit || {};
-var apiURL = "https://testimonialapi.toolcarton.com/api/";
+var apiURL = "https://64486933e7eb3378ca2e0f51.mockapi.io/api/users";
 
 // Provided funtion to create random number for API ids
 function getRandomInt() {
@@ -12,32 +12,76 @@ function getRandomInt() {
 // TODO: Remove previous HTML content from the card text. The content should be "" 
 function clearText() {
 	// Your code
+	document.querySelector("#name").innerHTML = ``;
+	document.querySelector("#text").innerHTML = ``;
 }
 
 // TODO: replace period by exclamation signs, add a new line, return processed text
 function processText(text){
       // Your Code
+	  const processedText = text.replaceAll('.', '!!!\n');
+	  
+	  return processedText;
  }
 
    // TODO: split text by a new line, create an array of sentences, return array
 function textToArray(text) {
 	// Your Code
+	const sentences = text.split('\n');
+	
+	return sentences;
 }
 
  // TODO: create for loop over text array. Append span tag and update code
  function appendText (textArray){
     // Your Code
-		 $("p").append($(`<span style = "display:block;">YOUR CODE</span>`));
+	let newParagraph = document.createElement('p');
+	for(let k=0; k<textArray.length; k++){
+		let appen = (`<span style = "display:block;">${textArray[k]}</span>`)
+	
+		 newParagraph.append(appen);
  }
+
+ return newParagraph;
+}
+
 
 
 function updateClient( ) {
 
 	let id = getRandomInt() // https://developer.mozilla.org/en-US/docs/Web/API/fetch
-	 
+	 let title = '';
+	 let cardText = '';
+	 let cardImage = '';
 	clearText(); 
 	// PART 1. First just replace card title, card text and card image with new content
 	
+		fetch(apiURL)
+		.then(response => response.json())
+		.then(data => {
+			// Use the data retrieved from the API
+			// console.log(data[id].name);
+		console.log(data[id].message)
+		
+			let title = data[id].name;
+			cardText = data[id].message;
+			cardImage = data[id].avatar;
+			cardText = processText(cardText);
+	
+			cardText = textToArray(cardText);
+
+	appendText(cardText);	
+			document.querySelector("#name").innerHTML = `${title}`;
+        document.querySelector('#text').innerHTML = `${cardText}`;
+		document.querySelector('#image').src = `${cardImage}`;
+			
+		})
+		.catch(error =>{
+			console.error("There was an error fetching data:", error);
+		});
+
+
+		
 	// PART 2. Text processing steps - processText, textToArraay, appendText
 
 	// text = processText(text);
